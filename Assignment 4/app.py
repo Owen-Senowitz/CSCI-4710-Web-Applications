5075b528aed9c9e9f6251e5c1cd84bf80313b6ec
Owen Senowitz <owen.senowitz@gmail.com>
2023-03-14 20:14:14 -0400

    added table

--- Assignment 4/app.py
+++ Assignment 4/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from forms import ClassmateForm, DataProcessingForm
 from controllers import process_data, plot_data
+from models import read_classmate_data
+import pandas as pd
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a_random_secret_key'
@@ -9,6 +11,7 @@ app.config['SECRET_KEY'] = 'a_random_secret_key'
 def index():
     form = ClassmateForm()
     dp_form = DataProcessingForm()
+    classmates = read_classmate_data().to_dict('records')
 
     if form.validate_on_submit():
         process_data(form)
@@ -17,7 +20,7 @@ def index():
     if request.method == 'POST' and dp_form.validate():
         plot_data(dp_form.data_processing_method.data)
 
-    return render_template('index.html', form=form, dp_form=dp_form)
+    return render_template('index.html', form=form, dp_form=dp_form, classmates=classmates)
 
 if __name__ == '__main__':
     app.run(debug=True)
